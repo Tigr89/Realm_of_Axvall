fedbbd2292d30d20ed350084b8c61933ce33c914
unknown <h24emwe@axevalla.fhsk.se>
2026-01-13 15:32:36 +0100

    spawn enemy

--- Assets/Scripts/Enemy scripts/spawn.cs
+++ Assets/Scripts/Enemy scripts/spawn.cs
@@ -1,16 +1,25 @@
 using System.Collections;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 
 
 public class spawn : MonoBehaviour
 {
     public GameObject enemy;
-    
+    public int spawnrate = 20;
+    public GameObject player;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         transform.position = new Vector3(3.4f, -0.2f, 0);
-
+        for (var i = 0; i < spawnrate; i++)
+        {
+            float startposx = Random.Range(-8.0f, 8.0f);
+            float startposy = Random.Range(-8.0f, 8.0f);
+            Instantiate(enemy, new Vector3(startposx, startposy, 0), Quaternion.identity);
+           // stats.GetComponent<statisticsManager>().healthyCount++;
+        }
     }
 
     // Update is called once per frame
@@ -20,11 +29,13 @@ public class spawn : MonoBehaviour
     }
     IEnumerator SpawnEnemy()
     {
-        
-        yield return new WaitForSeconds(2);
-        Instantiate(enemy,new Vector3(3.4f, -0.2f, 0),transform.rotation);
-        yield return null;
+        float SpawnX = player.transform.position.x + 3;
+        float SpawnY = player.transform.position.y + 1;
 
+        yield return new WaitForSeconds(2);
+        Instantiate(enemy,new Vector3(SpawnX, SpawnY, 0),transform.rotation);
+        yield return null;
+       
     }
     public void startspawn()
     {
