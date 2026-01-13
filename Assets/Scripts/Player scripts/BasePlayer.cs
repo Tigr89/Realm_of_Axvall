d0e5cde003be78685e87bf32f7a181fe911c5a45
Limpaank <103106399+Limpaank@users.noreply.github.com>
2026-01-13 13:52:18 +0100

    player base stat class test
    verkar funka, osäker på hur exakt det ska implementeras i nuläget

---
+++ Assets/Scripts/Player scripts/BasePlayer.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class BasePlayer
+{
+    public int health;
+
+    public int strengh;
+    public int inteligence;
+    public int dexterity;
+
+    public int experience;
+
+    public int Health
+    {
+        get { return health; }
+        set { health = value; }
+    }
+
+    public int Strengh
+    {
+        get { return strengh; }
+        set { strengh = value; }
+    }
+
+    public int Inteligence
+    {
+        get { return inteligence; }
+        set { inteligence = value; }
+    }
+
+    public int Dexterity
+    {
+        get { return dexterity; }
+        set { dexterity = value; }
+    }
+
+    public int Experience
+    {
+        get { return experience; }
+        set { experience = value; }
+    }
+
+
+}
+
+
---
+++ Assets/Scripts/Player scripts/playertest.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class playertest : BasePlayer
+{
+    public playertest()
+    {
+        health = 100;
+        strengh = 10;
+        inteligence = 10;
+        dexterity = 10;
+        experience = 0;
+    }
+}
