43f9c44cdd694477911b084b9f1d4a0628f18055
Pancake <sspel25adni@axevalla.fhsk.se>
2026-01-13 16:00:29 +0100

    inte klar

--- Assets/Scripts/enemy/Base/Enemy.cs
+++ Assets/Scripts/enemy/Base/Enemy.cs
@@ -1,16 +1,37 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
+     
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Vector2 target;
+    private Vector2 position;
+    private Vector3 startingPosition;
+
+    private void Start()
     {
-        
+        startingPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private Vector3 GetRoamingPosition()
     {
+        Vector3 ReturnPosition = Vector3.zero;
+
+        //Slumpa ny position
+        float Targetx = Random.Range(-4, +4);
+        float Targety = Random.Range(-4, +4);
+
+        ReturnPosition = new Vector3(Targetx, Targety, 0);
+
         
+
+
+        return ReturnPosition;
     }
+    private void Update()
+    {
+        //transform.position means the one with the script's position
+        //next assignment I will work with making the enemy actually move in x area
+    }
+
 }
