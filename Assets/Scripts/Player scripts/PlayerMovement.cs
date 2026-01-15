6bd50d3ce86579763f52c9e1bd174d994b0e1946
Limpaank <103106399+Limpaank@users.noreply.github.com>
2026-01-15 11:23:58 +0100

    ändrat tillbaka test movement ändringar

--- Assets/Scripts/Player scripts/PlayerMovement.cs
+++ Assets/Scripts/Player scripts/PlayerMovement.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : PlayerStats
+public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 3f;
     private Rigidbody2D rb;
@@ -10,7 +10,6 @@ public class PlayerMovement : PlayerStats
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        changeClass();
         //Creating a reference to the Rigidbody2D-component.
         rb = GetComponent<Rigidbody2D>();
     }
@@ -18,31 +17,16 @@ public class PlayerMovement : PlayerStats
     // Update is called once per frame
     void Update()
     {
-        TempMoveInput();
+        
         //This line of code handles movement
-        rb.linearVelocity = moveSpeed * moveInput * dexterity/10;
+        rb.linearVelocity = moveSpeed * moveInput;
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            levelUp();
-            //experience += 50;
-        }
-
-        if (experience >= experienceToNextLevel)
-        {
-            levelUp();
-        }
     }
 
     //This function returns a value between -1 and 1 in both X and Y-axis depending on player input
     public void Movement(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
-    }
-
-    void TempMoveInput()
-    {
-        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
     }
 
 }
